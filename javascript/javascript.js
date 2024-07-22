145c951223a3d119ce19447adc64e885314e2464
Kavibharathi0411 <kavibharathi811@gmail.com>
2024-07-22 17:34:13 +0530

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,26 @@
+ function red1(){
+    document.getElementsByClassName("red1")[0].style.backgroundColor="red";
+}
+function red2(){
+    document.getElementsByClassName("red2")[0].style.backgroundColor="red";
+}
+function red3(){
+    document.getElementsByClassName("red3")[0].style.backgroundColor="red";
+}
+function red4(){
+    document.getElementsByClassName("red4")[0].style.backgroundColor="red";
+}
+function red5(){
+    document.getElementsByClassName("red6")[0].style.backgroundColor="red";
+}
+function red6(){
+    document.getElementsByClassName("red1")[0].style.backgroundColor="red";
+}
+function green1(){
+    document.getElementsByClassName("green1")[0].style.backgroundColor="red";
+}
+function green2(){
+    document.getElementsByClassName("green2")[0].style.backgroundColor="red";
+}
+ 
+ 
